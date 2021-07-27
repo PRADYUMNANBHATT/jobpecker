b4fe58684a210bbe88af0e056a9f444adfc5314c
PRADYUMNANBHATT <kpradyumnan@gmail.com>
2021-07-27 22:06:50 +0530

    first commit

---
+++ web/app.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+  const pizza = document.querySelector(".pizza");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li ");
+
+  pizza.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
+
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = "navLinkFade 0.5s ease forwards ${index/7+1.3}s";
+      }
+    });
+    //piza animation
+    pizza.classList.toggle("toggle");
+  });
+};
+
+navSlide();
